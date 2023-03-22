bddda79e62d2394a27609e1bf0f14204717a8132
kkawaguchi <kkawaguchi@artis.co.jp>
2023-03-23 06:44:06 +0900

    add comment. #8672

--- lib/samly/router_util.ex
+++ lib/samly/router_util.ex
@@ -119,7 +119,9 @@ defmodule Samly.RouterUtil do
   def redirect(conn, status_code, dest) do
     Logger.error("#### RouterUtil#redirect")
     Logger.error("# dest = #{inspect(dest)}")
+    Logger.error("# dest.replace = #{inspect(String.replace(dest, "%25", "%")}")
 
+    # TODO!!: SAMLResponse の値を URL デコードすればうまくいきそうな気がするが。。
     conn
     |> Conn.put_resp_header("location", dest)
     |> Conn.send_resp(status_code, "")
